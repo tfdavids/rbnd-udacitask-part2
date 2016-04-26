013da472282f353760ed1a14398812192c5b78e0
Thomas Davids <tfdavids@gmail.com>
2016-04-26 08:57:48 -0700

    refactor out format_priority

--- lib/listable.rb
+++ lib/listable.rb
@@ -7,4 +7,11 @@ module Listable
         dates << " -- " + end_date.strftime("%D") if end_date
         return dates
     end
+    def format_priority(priority)
+        value = " ⇧" if priority == "high"
+        value = " ⇨" if priority == "medium"
+        value = " ⇩" if priority == "low"
+        value = "" if !priority
+        return value
+      end
 end
--- lib/todo.rb
+++ lib/todo.rb
@@ -7,18 +7,11 @@ class TodoItem
     @due = options[:due] ? Date.parse(options[:due]) : options[:due]
     @priority = options[:priority]
   end
-  def format_priority
-    value = " ⇧" if @priority == "high"
-    value = " ⇨" if @priority == "medium"
-    value = " ⇩" if @priority == "low"
-    value = "" if !@priority
-    return value
-  end
   def details
     date = format_date(@due)
     date = "No due date" if !date
     format_description(@description) + "due: " +
     date +
-    format_priority
+    format_priority(@priority)
   end
 end
